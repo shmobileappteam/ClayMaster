8ffa90d0d343d1baa809c162f1657e1ed7814299
sheikhabdulmoiz <abdulmoiz15000@gmail.com>
2025-12-01 17:23:44 +0500

    added info pList

--- src/screens/Main/Subscription/SubscribtionSuccessScreen.js
+++ src/screens/Main/Subscription/SubscribtionSuccessScreen.js
@@ -1,41 +1,32 @@
-import {
-    StyleSheet,
-} from 'react-native';
+import { StyleSheet } from 'react-native';
 //-----
 import { SafeAreaWrapper } from '../../../atomComponents';
 import SuccessMessage from '../../../components/SuccessMessage/SuccessMessage';
 import { CommonActions } from '@react-navigation/native';
 
-
-
 const SubscribtionSuccessScreen = ({ navigation }) => {
-
-    return (
-        <SafeAreaWrapper
-            keyboardAvoiding
-            contentStyle={{ justifyContent: 'center', alignItem: 'center' }}
-        >            <SuccessMessage
-                title="Subscription Activated!"
-                message="Welcome to the premium experience! Your subscription is now active."
-                buttonLabel="Continue"
-                onPress={() => {
-
-
-                    navigation.dispatch(
-                        CommonActions.reset({
-                            index: 0,
-                            routes: [{ name: 'BottomTabs' }],
-                        }),
-                    );
-                }}
-            />
-
-        </SafeAreaWrapper>
-    );
+  return (
+    <SafeAreaWrapper
+      keyboardAvoiding
+      contentStyle={{ justifyContent: 'center', alignItem: 'center' }}
+    >
+      <SuccessMessage
+        title="Subscription Activated!"
+        message="Welcome to the premium experience! Your subscription is now active."
+        buttonLabel="Continue"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{ name: 'BottomTabs' }],
+            }),
+          );
+        }}
+      />
+    </SafeAreaWrapper>
+  );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default SubscribtionSuccessScreen;
